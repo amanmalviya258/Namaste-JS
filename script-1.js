44c9f2396b5c0775760c783bfa792d46d6fd4fd6
Aman <amanmalviya258@gmail.com>
2023-06-20 12:47:20 +0530

    first commit

---
+++ script-1.js
@@ -0,0 +1,66 @@
+// This will pint Namaste-JS and then 10
+// var x = 10;
+// function getName(){
+//     console.log("Namaste-JS");
+// }
+// getName();
+// console.log(x);
+
+//This will print undefined and then Namaste-Js
+// getName();
+// console.log(x);
+// var x = 10;
+// function getName(){
+//     console.log("Namaste-JS");
+// }
+
+// This will throw the error of x not defined
+// getName();
+// console.log(x);
+// function getName() {
+//   console.log("Namaste-JS");
+// }
+
+// This will print the function itself
+// function getName() {
+//     console.log("Namaste-JS");
+// }
+// console.log(getName);
+
+//    This will also print the function  itself
+// console.log(getName);
+// function getName() {
+//     console.log("Namaste-JS");
+// }
+
+//this will print Namaste-JS then Undefined then the function itSelf
+//  getName();
+// console.log(x);
+// console.log(getName);
+
+// var x = 10;
+// function getName(){
+//         console.log("Namaste-JS");
+//     }
+
+//This will print getName is not a function i.e we are  using this before even initialising
+
+// getName();
+// console.log(x);
+// console.log(getName);
+// var x = 10;
+// var getName = () => console.log("Namaste-JS"); //here this function declared using fat-Arrow Function will behave like a variable
+
+//This will print getName is not a function i.e we are  using this before even initialising
+
+// getName();
+// console.log(x);
+// console.log(getName);
+// var x = 10;
+// var getName = function(){ console.log("Namaste-JS")} //here this function declared using fat-Arrow Function will behave like a variable
+
+
+//This will print undefined and Namaste-JS
+// console.log(x);
+// var x = 10;
+// (() => console.log("Namaste-JS"))(); //we need to put the fat arrow function in parantheses in-order to run the console.log reason meko nhi samaj aaya 
