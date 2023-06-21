c54d2bd08a11182d56bf00ffc52e117c9d0141f6
Aman Malviya <82308626+amanmalviya258@users.noreply.github.com>
2023-06-21 14:28:26 +0530

    Add files via upload

---
+++ temporalDeadZone.js
@@ -0,0 +1,22 @@
+// console.log(a); // ReferenceError: Cannot access 'a' before initialization
+// console.log(b); // prints undefined as expected
+// let a = 10;
+// console.log(a); // 10
+// var b = 15;
+// console.log(window.a); // undefined
+// console.log(window.b); // 15
+
+// let a = 20; //not possible this will throw a syntax error of identifier a is already been declared ie we can't redeclare let 
+
+// var a = 20; // // we cant use the same name in case of let even when we use var  will throw a syntax error of identifier a is already been declared
+
+// const b; // // cont declarations must be initialised
+// console.log(b);
+// let a ;
+// b = 1000;
+//  a = 10;
+//  console.log(a);
+//  a = 100;
+//  console.log(a);
+// const z = 10
+// z= 1000 //TypeError: Assignment to constant variable.
